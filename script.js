6ab0cf0c92f7f5eda5ecaeebb56f0dab00028a67
Lan Luong <lan.luong@student.csulb.edu>
2019-03-28 19:50:59 -0700

    button

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 function myFunction() {
-    var message, x;
+    var message, x, y;
     message = document.getElementById("message");
     message.innerHTML = "";
     x = document.getElementById("Number 1").value;
-    try { 
-      if(x > 100)   throw "Invalid input, please try again.";
-      if(x < 2)  throw "Invalid input, please try again.";
-    }
-    catch(err) {
-      message.innerHTML = err;
-    }
-  }
-  
-  var message, y;
-    message = document.getElementById("message");
-    message.innerHTML = "";
     y = document.getElementById("Number 2").value;
     try { 
       if(x > 100)   throw "Invalid input, please try again.";
       if(x < 2)  throw "Invalid input, please try again.";
+      if(y > 100)   throw "Invalid input, please try again.";
+      if(y < 2)  throw "Invalid input, please try again.";
     }
     catch(err) {
       message.innerHTML = err;
