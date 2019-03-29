274df8990e0a095e62cbbfbca38eb2dbe42a278a
Lan Luong <lan.luong@student.csulb.edu>
2019-03-28 21:30:19 -0700

    initial commit

--- script.js
+++ script.js
@@ -15,30 +15,29 @@ function myFunction() {
     }
   }
 
- // function myFunction() {
- //   var i = 0;
-  //  var j = 0;
+  function printPrime() {
+    var i, j;
 
-  //  a = document.getElementById('Number 1').value;
- //   b = document.getElementById('Number 2').value;
+    i = document.getElementById('Number 1').value;
+    j = document.getElementById('Number 2').value;
 
-    //loop till i equals to $limit
- //   for (i = 1; i <= limit; i++) {
- //       c = 0;
+        //loop till i equals to $limit
+     for (i = 2; i <= j; i++) {
+        c = 0;
 
-  //      for (j = 1; j <= i; j++) {
+        for (j = 2; j <= i; j++) {
             // % modules will give the reminder value, so if the reminder is 0 then it is divisible
- //           if (i % j == 0) {
+            if (i % j == 0) {
                 //increment the value of c
- //               c++;
- //           }
- //       }
+                c++;
+            }
+        }
 
         //if the value of c is 2 then it is a prime number
         //because a prime number should be exactly divisible by 2 times only (itself and 1)
- //       if (c == 2) {
- //           document.getElementById("message").insertAdjacentHTML('beforeend', i + '<br>');
- //       }
-//    }
-//}
+        if (c == 2) {
+            document.getElementById("message").insertAdjacentHTML('beforeend', i + '<br>');
+        }
+    }
+}
 
