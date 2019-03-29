376cdded45d93978fa2b40459bb030ddb98547d3
Lan Luong <lan.luong@student.csulb.edu>
2019-03-28 20:24:22 -0700

    testing

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ function myFunction() {
       message.innerHTML = err; 
     }
   }
+
+ // function myFunction() {
+ //   var i = 0;
+  //  var j = 0;
+
+  //  a = document.getElementById('Number 1').value;
+ //   b = document.getElementById('Number 2').value;
+
+    //loop till i equals to $limit
+ //   for (i = 1; i <= limit; i++) {
+ //       c = 0;
+
+  //      for (j = 1; j <= i; j++) {
+            // % modules will give the reminder value, so if the reminder is 0 then it is divisible
+ //           if (i % j == 0) {
+                //increment the value of c
+ //               c++;
+ //           }
+ //       }
+
+        //if the value of c is 2 then it is a prime number
+        //because a prime number should be exactly divisible by 2 times only (itself and 1)
+ //       if (c == 2) {
+ //           document.getElementById("message").insertAdjacentHTML('beforeend', i + '<br>');
+ //       }
+//    }
+//}
+
